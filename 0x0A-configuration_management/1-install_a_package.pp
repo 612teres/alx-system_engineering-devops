5fc6586522647ff46441813ee5a6a89cc53b789b
612teres <612teres@gmail.com>
2024-05-07 01:48:30 +0300

    Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installs flask from pip3.
+# Install Flask version 2.1.0 using pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure python3-pip is installed first
 }
