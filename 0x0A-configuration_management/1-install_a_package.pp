6b65f975aa88668e56965c6777bd67d9ba286b5b
612teres <612teres@gmail.com>
2024-05-09 00:48:57 +0300

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install Flask version 2.1.0 using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'], # Ensure python3-pip is installed first
+# install puppet-lint
+package{'puppet-lint':
+ensure   => '2.5.0',
+provider => 'gem'
 }
